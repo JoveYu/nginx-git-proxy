c4c8897492cf1128e22b7391f6b8e48b70d76d71
Jove Yu <yushijun110@126.com>
2025-07-07 18:39:23 +0800

    fix os.execute return error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
+[ -e /run/fcgiwrap.socket ] && rm -f /run/fcgiwrap.socket
 fcgiwrap -s unix:/run/fcgiwrap.socket > /dev/null 2>&1 &
+
 sed -i "s/__NGINX_GIT_DOMAIN__/${NGINX_GIT_DOMAIN}/g" /etc/nginx/conf.d/*.conf
 sed -i "s/__NGINX_CORS_DOMAIN__/${NGINX_CORS_DOMAIN}/g" /etc/nginx/conf.d/*.conf
+
 exec "$@"
